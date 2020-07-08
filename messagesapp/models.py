c14ba22d338b6a3e81c72d90ad26e9072a027e39
Patrick Buzzo <pbuzzo04@gmail.com>
2020-07-07 21:02:23 -0500

    commit comment model

--- messagesapp/models.py
+++ messagesapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from postapp.models import Post
+from userapp.models import User
+from datetime import datetime, timedelta
 
-# Create your models here.
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=datetime.now, blank=True)
+    post = models.ForeignKey(TwitterUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
