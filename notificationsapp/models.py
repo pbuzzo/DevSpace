64e918f7446dc93b7f4c6c376434d1aa9c40bf07
Babatunde Odumosu <tudy4300@pop-os.localdomain>
2020-07-08 11:07:24 -0500

    Basic Models

--- notificationsapp/models.py
+++ notificationsapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.utils import timezone
+ 
 # Create your models here.
+class Notifications(models.Model):
+    data_created = models.DateTimeField(default=timezone.now)
+    to_user = models.BooleanField(default=False)
+    from_user = models.BooleanField(default=False)
