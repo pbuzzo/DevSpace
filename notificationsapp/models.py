205824de27fbceb1c9f13e042afc8f858b6b3375
Patrick Buzzo <pbuzzo04@gmail.com>
2020-07-09 21:35:07 -0500

    commit functional frontend signup

--- notificationsapp/models.py
+++ notificationsapp/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 from django.utils import timezone
-<<<<<<< HEAD
-from postapp.models import Post
-from userapp.models import Developer
- 
-=======
 from userapp.models import Developer
 from postapp.models import Post
 
->>>>>>> ac90dc9d23858638dd7c22070265aa4afe876142
 # Create your models here.
 class Notifications(models.Model):
     data_created = models.ForeignKey(Post, on_delete=models.CASCADE)
