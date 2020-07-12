15ec9cadeb78526be71ec5951574ef38b86cb000
Qu33nB <bdcotton52@gmail.com>
2020-07-11 22:33:05 -0500

    Updated user model and made migrations

--- resumeapp/admin.py
+++ resumeapp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from resumeapp.models import Resume, Education, Employment, References, Details
 
-admin.site.register(Resume, Education, Employment, References, Details)
+admin.site.register(Resume)
+admin.site.register(Education)
+admin.site.register(Employment)
+admin.site.register(References)
+admin.site.register(Details)
