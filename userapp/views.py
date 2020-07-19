efdf7d12e84805cc5e884b2b28de8c0c7d8a9c11
Qu33nB <bdcotton52@gmail.com>
2020-07-19 16:41:41 -0500

    Minor changes

--- userapp/views.py
+++ userapp/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.views.generic import View
 
 from userapp.models import Developer
 from userapp.forms import SignInForm, SignUpForm
-# from postapp.models import Post
+from postapp.models import Post
 
-def index(request):
-    info = Developer.objects.all()
-    return render(request, 'index.html', {'info': info})
 
+class IndexView(View):
+    def get(self, request):
+        html = 'index.html'
+        info = Developer.objects.all()
+        posts = Post.objects.filter(author=request.user.id)
+
+        return render(
+            request,
+            html,
+            {'info': info}
+        )
 
 # @login_required
 # def signed_in(request, username):
@@ -30,7 +39,6 @@ def signin(request):
             if user:
                 login(request, user)
                 return HttpResponseRedirect(reverse('home'))
-            return HttpResponse(f'Please return to the form and fix the following errors: {form.errors}')
 
     form = SignInForm()
     return render(request, htm, {'form': form})
