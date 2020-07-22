c1dcc8305363dcbb63f6bbabad966af0494c61ae
Patrick Buzzo <pbuzzo04@gmail.com>
2020-07-22 07:43:55 -0500

    update dev page and user cards

--- userapp/urls.py
+++ userapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('developer/<int:id>', views.DeveloperView.as_view(), name='dev_page'),
     path('developer/<int:id>/edit/', views.edit_user, name='edit_user'),
-    path('signin/', views.signin),
+    path('signin/', views.signin, name='signin'),
     path('signup/', views.signup),
     path('signout/', views.signout),
     # path('<str:username>/', views.signed_in, name='signed_in'),
